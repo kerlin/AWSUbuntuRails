1a0dff273b7198b8bcd976e79348d63f3005c3b1
Chris Kerlin <chris.kerlin@gmail.com>
2016-05-31 17:04:10 -0400

    Update setup.sh
    need -y else no c compiler

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 sudo apt-get update
 
 # Install dependencies
-sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 
 # install rbenv
 cd
