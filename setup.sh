60d09bf165289c5894b6ca090bbfda5ae0a21f45
Chris Kerlin <chris.kerlin@gmail.com>
2016-06-08 14:43:25 -0400

    Update setup.sh
    edit

--- setup.sh
+++ setup.sh
@@ -7,14 +7,14 @@ sudo apt-get update
 sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 
 # install rbenv
-cd
+cd $HOME
 git clone git://github.com/sstephenson/rbenv.git .rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
-source ~/.bash_profile
+. ~/.bash_profile
 
 # install latest Ruby (May 2016)
 rbenv install -v 2.3.1
